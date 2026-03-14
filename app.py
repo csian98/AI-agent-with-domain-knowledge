665d15223e67e48e118851b8ad893081416fb928
Viraj Bansal <virajmohit7@gmail.com>
2026-03-14 12:58:32 -0700

    fixed requirements

--- app.py
+++ app.py
@@ -1,56 +1,61 @@
 import os, sys
+
 sys.path.append("pylib/")
 
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 
-from qdrant_engine import QdrantEngine
-from llm_engine import OllamaLLMEngine, OpenAILLMEngine, AnthropicLLMEngine
+from pylib.qdrant_engine import QdrantEngine
+from pylib.llm_engine import OllamaLLMEngine, OpenAILLMEngine, AnthropicLLMEngine
 
-from snowflake_util import *
+from pylib.snowflake_util import *
 
 app = FastAPI()
-qdrant = QdrantEngine(db_path="qdrant",
-                      collection_name="chunks",
-                      embedding_model="BAAI/bge-small-en-v1.5")
+qdrant = QdrantEngine(
+    db_path="qdrant", collection_name="chunks", embedding_model="BAAI/bge-small-en-v1.5"
+)
 
-ollama_llm_engine = OllamaLLMEngine(model="gpt-oss:20b", stream=False)
+# ollama_llm_engine = OllamaLLMEngine(model="gpt-oss:20b", stream=False)
 openai_llm_engine = OpenAILLMEngine(model="gpt-5-nano", stream=False)
 anthropic_llm_engine = AnthropicLLMEngine(model="claude-haiku-4-5", stream=False)
+
 
 def chunk_format(chunk_info: dict) -> str:
     return f"[{chunk_info['source']}] {chunk_info['content']}"
 
+
 class ChatRequest(BaseModel):
     model: str = "gpt-oss:20b"
-    # "gpt-oss:20b", "gpt-5-nano", "calude-haiku-4-5"
+    # "gpt-oss:20b", "gpt-5-nano", "claude-haiku-4-5"
     embed: str = "qdrant"
     # "qdrant", "snowflake"
     prompt: str
 
+
 @app.post("/api/generate")
 def generate(request: ChatRequest):
+    domain_knowledge = []
     if request.embed == "qdrant":
         domain_knowledge = qdrant.search(request.prompt, 10)
     elif request.embed == "snowflake":
         conn = get_connection()
-        domain_knowledge = similarity_search(
-            conn, request.prompt, 10)
+        domain_knowledge = similarity_search(conn, request.prompt, 10)
 
     domain_knowledge = list(map(chunk_format, domain_knowledge))
-    
+
     prompt = (
         "You are an LLM agent created to assist users.\n"
         "Actively utilize your prio knowledge to appropriately answer user queries.\n"
         "Given Domain Knowledge:\n"
     )
 
-    prompt += '\n'.join(domain_knowledge)
+    prompt += "\n".join(domain_knowledge)
     prompt += f"\nUser Query: {request.prompt}"
 
     if request.model == "gpt-oss:20b":
-        text = ollama_llm_engine.generate(prompt)
+        # text = ollama_llm_engine.generate(prompt)
+        text = "Ollama LLM Engine is currently disabled."
     elif request.model == "gpt-5-nano":
         text = openai_llm_engine.generate(prompt)
     elif request.model == "claude-haiku-4-5":
@@ -59,5 +64,6 @@ def generate(request: ChatRequest):
         return {"response": f"{request.model} not supported"}
 
     return {"response": text}
+
 
 # curl -X POST http://10.10.50.5:4444/api/generate -H "Content-Type: application/json" -d '{"model": "gpt-oss:20b", "embed": "qdrant","prompt": "hello"}'
--- pylib/qdrant_upload_main.py
+++ pylib/qdrant_upload_main.py
@@ -1,4 +1,5 @@
 import os, sys
+
 sys.path.append("pylib/")
 import snowflake.connector
 
@@ -7,27 +8,31 @@ from snowflake_util import *
 
 TABLE_NAME = "DOCUMENT_EMBEDDINGS"
 
+
 def get_data(conn):
     with conn.cursor() as cur:
         cur.execute(f"SELECT chunk_id, source, content FROM {TABLE_NAME}")
         df = cur.fetch_pandas_all()
-        
+
     return df
 
-if __name__ == "__main__":
+
+def main():
     qdrant = QdrantEngine("qdrant", "chunks", "BAAI/bge-small-en-v1.5")
     qdrant.reset()
-    
+
     conn = get_connection()
     df = get_data(conn)
 
     size = len(df)
     for idx, row in df.iterrows():
-        print(f"\r[{idx + 1}/{size}] Insert into qdrant...", end='')
+        print(f"\r[{idx + 1}/{size}] Insert into qdrant...", end="")
 
-        qdrant.upload(row["CHUNK_ID"],
-                      row["SOURCE"],
-                      row["CONTENT"])
+        qdrant.upload(row["CHUNK_ID"], row["SOURCE"], row["CONTENT"])
 
     print("\nComplete uploading...")
     qdrant.close()
+
+
+if __name__ == "__main__":
+    main()
